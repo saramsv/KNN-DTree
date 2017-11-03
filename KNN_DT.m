8bea2b5ad47817be101601f81761932f49bbe111
saramsv <sara.mousavi.ch@gmail.com>
2017-11-03 10:58:47 -0400

    tested the my KNN function with the knnsearch matlab built in function

--- KNN_DT.m
+++ KNN_DT.m
@@ -28,48 +28,45 @@ sigma = std(cleaned_data);
 tvdata = cleaned_data(1:600,:);%10,:)%
 %validate_data = cleaned_data(426:562,:);%430,:)%
 test_data = cleaned_data(600:699,:);
-K = [2,3,4];%,5,6,7,8,16,32];%[5, 9];%
-% acc = [];
-% fold = 10;
-% aves = [];
-% for i = 1:length(K)
-%     ave_fold = 0;
-%     for j = 1:fold
-%         validate_data = tvdata((j-1)*60+1 : j*60 , :);
-%         if j == 1
-%             train_data = tvdata(j*60+1:600,:);
-%         elseif j == 10
-%             train_data = tvdata(1:540,:);
-%         else
-%             train_data = [tvdata(1:(j-1)*60, :); tvdata(j*60 +1:600, :)];
-%         end
-%         classes = k_nearest_neighbors(train_data, validate_data, K(i));
-%         Accuracy = performance(validate_data(:,10),classes);
-%         ave_fold = ave_fold + Accuracy;
-%         acc = [acc ; [K(i), Accuracy]];
-%     end
-%     aves = [aves ave_fold/fold];
-% end
-% acc;
+K = [2];%,3,4,5,6,7,8,16,32];%[5, 9];%
+acc = [];
+fold = 1;
+aves = [];
+for i = 1:length(K)
+    ave_fold = 0;
+    for j = 1:fold
+        validate_data = tvdata((j-1)*60+1 : j*60 , :);
+        if j == 1
+            train_data = tvdata(j*60+1:600,:);
+        elseif j == 10
+            train_data = tvdata(1:540,:);
+        else
+            train_data = [tvdata(1:(j-1)*60, :); tvdata(j*60 +1:600, :)];
+        end
+        classes = k_nearest_neighbors(train_data, validate_data, K(i));
+        %knnsearch(train_data, validate_data)
+        Accuracy = performance(validate_data(:,10),classes);
+        ave_fold = ave_fold + Accuracy;
+        acc = [acc ; [K(i), Accuracy]];
+    end
+    aves = [aves ave_fold/fold];
+end
 % [maxK k_best] = max(aves);
 % plot(acc(:,1), acc(:,2), '.b')
 % ylim([0 1.10])
-%knnsearch
-names = ["ClumpThickness";"UniformityofCellSize";"UniformityofCellShape";"MarginalAdhesion";"SingleEpithelialCellSize";"BareNuclei";"BlandChromatin";"NormalNucleoli";"Mitoses"];
-%[mytree, root] = tree;
-for i = 1:length(K)
-    %K(i)
-    [result_tree, name, real_data, classified_data, structure] = classify_by_DT(tvdata, K(i));
-    %disp(result_tree.tostring);
-    p = sum(real_data(:,10) ~= classified_data(:,10));
-    error = p/size(real_data,1)
-    structure;
-    size(structure);
-    [t_real_data, t_classified_data] = classify_test_data(test_data, structure);
-    p = sum(t_real_data(:,10) ~= t_classified_data(:,10));
-    test_error = p/size(t_real_data,1)
-    %structure
-end
+% xlim([acc(1,1)-1 acc(end,1)+1])
+
+
+% for i = 1:length(K)
+%     %K(i)
+%     [result_tree, name, real_data, classified_data, structure] = classify_by_DT(tvdata, K(i));
+%     %disp(result_tree.tostring);
+%     p = sum(real_data(:,10) ~= classified_data(:,10));
+%     error = p/size(real_data,1);
+%     [t_real_data, t_classified_data] = classify_test_data(test_data, structure);
+%     p = sum(t_real_data(:,10) ~= t_classified_data(:,10));
+%     test_error = p/size(t_real_data,1);
+% end
 
 function classes = k_nearest_neighbors(train_data, validate_data,k)
     classes = [];
@@ -81,7 +78,7 @@ function classes = k_nearest_neighbors(train_data, validate_data,k)
         for j = 1: train_num
             diff = validate_data(i, 1:9)-train_data(j, 1:9);
             dis = sqrt(diff*diff');
-            distances = [distances ; [dis, train_data(j,10)]];
+            distances = [distances ; [dis, train_data(j,10), j]];
         end
         distances;
         [~,idx] = sort(distances(:,1));
@@ -124,7 +121,6 @@ function [temp , root, real_data, classified_data, structure] = classify_by_DT(d
         real_data  = [];
         structure = [];
         depth = 1;
-        %max_depth = max_depth;
     else
         depth = depth + 1;
     end
@@ -185,6 +181,7 @@ function [temp , root, real_data, classified_data, structure] = classify_by_DT(d
         structure = [structure, [0;0]];
         
         return;
+    %not leaf
     else
         c = strcat(names(impurity_measures(1,col)),'>=');
         c = cellstr(strcat(c, num2str(impurity_measures(2,col))));
@@ -229,103 +226,3 @@ function [t_real_data, t_classified_data, col] = classify_test_data(data, struct
     [t_real_data, t_classified_data, col] = classify_test_data(branch2, structure, t_real_data, t_classified_data,col);    
 
 end
-    % MOA::THE FOLLOWING LINE IS THE ONLY LINE THAT NEEDS TO CHANGE
-%     if size(data,1)==size(branch1,1) | size(branch2,1) <= 10
-%         p1 = sum(branch1(:,10)==2);
-%         p1 = p1/size(branch1,1);
-%         p2 = sum(branch1(:,10)==4);
-%         p2 = p2/size(branch1,1);
-%         class = ones(1,size(branch1,1));
-%         if p1 > p2
-%             class = 2*class; 
-%         else
-%             class = 4*class; 
-%         end
-%         branch1(:,10) = class';
-%         result = [result branch1];
-%         [temp , root] = tree;
-%         temp = temp.addnode(root, branch1)
-%         return;
-%     end
-%     
-%     if size(data,1)==size(branch2,1) |size(branch1,1) <= 10
-%         p1 = sum(branch2(:,10)==2);
-%         p1 = p1/size(branch2,1);
-%         p2 = sum(branch2(:,10)==4);
-%         p2 = p2/size(branch2,1);
-%         class = ones(1,size(branch2,1));
-%         if p1 > p2
-%             class = 2*class; 
-%         else
-%             class = 4*class; 
-%         end
-%         branch2(:,10) = class';
-%         result = [result branch2];
-%         [temp root] = tree;
-%         temp = temp.addnode(root, branch2)
-%         return;
-%     end
-    
-
-    %c = strcat(names(impurity_measures(1,col)),'>=');
-    %c = cellstr(strcat(c, num2str(impurity_measures(2,col))));
-    
-    % Dar calle aval
-     %[temp, root]= tree(c)
-%     [temp, child_target1] = temp.addnode(1, branch1);
-%     
-%     % dar calle dovom
-%     temp = tree(c)
-%     [temp, child_target2]= temp.addnode(1, branch2);
-%     
-%     
-% 
-%     mytree = mytree.graft(root, temp);
-
-    % If it's a leaf
-%     if impurity_measures(3,col) <=0.02 | length(outs) == num_features
-%         return;
-%     end
-%     
-%     if impurity_measures(3,col) > 0.02
-%         [child1_target, root]= classify_by_DT(branch1, names, features, outs, result);
-%         [child2_target, root] = classify_by_DT(branch2, names, features, outs, result);
-%         temp  = temp.addnode(root, child1_target);
-%         temp = temp.addnode(root, child2_target);
-%     end
-
-    
-    %t.get(3)
-            
-    %end
-%end
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
