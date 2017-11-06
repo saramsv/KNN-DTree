efa09beff571fed33d8ab292d0ce8cd1144e50b3
saramsv <sara.mousavi.ch@gmail.com>
2017-11-06 11:54:48 -0500

    Final version

--- KNN_DT.m
+++ KNN_DT.m
@@ -1,5 +1,5 @@
 data = importdata('breast-cancer-wisconsin.csv');
-%cleaning
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%cleaning%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 cleaned_data = data;
 for i =1: size(cleaned_data,1)
     if isnan(cleaned_data(i,7))
@@ -25,15 +25,19 @@ ave = mean(cleaned_data);
 sigma = std(cleaned_data);
 % size(cleaned_data);
 %train_data = cleaned_data(1:600,:);%10,:)%
-tvdata = cleaned_data(1:600,:);%10,:)%
+tvdata = cleaned_data(1:600,:);% train and validate data
 %validate_data = cleaned_data(426:562,:);%430,:)%
 test_data = cleaned_data(600:699,:);
-K = [2];%,3,4,5,6,7,8,16,32];%[5, 9];%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% KNN %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ K = [2,3,4,5,6,7,8,16,32];
 acc = [];
-fold = 1;
+fold = 10;
 aves = [];
 for i = 1:length(K)
     ave_fold = 0;
+    fprintf('k = %u\n', K(i))
     for j = 1:fold
         validate_data = tvdata((j-1)*60+1 : j*60 , :);
         if j == 1
@@ -51,23 +55,64 @@ for i = 1:length(K)
     end
     aves = [aves ave_fold/fold];
 end
-% [maxK k_best] = max(aves);
-% plot(acc(:,1), acc(:,2), '.b')
-% ylim([0 1.10])
-% xlim([acc(1,1)-1 acc(end,1)+1])
+[maxK k_best] = max(aves);
+plot(acc(:,1), acc(:,2), '.b')
+ylim([0 1.05])
+xlim([acc(1,1)-1 acc(end,1)+1])
+%applying knn on the test_data
+classes = k_nearest_neighbors(tvdata, test_data, K(k_best));
+Accuracy = performance(test_data(:,10),classes)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Decision tree %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+min_error = 1;
+for i = 1:length(K)
+    [result_tree, name, real_data, classified_data, structure] = classify_by_DT(tvdata, K(i));
+    %disp(result_tree.tostring);
+    p = sum(real_data(:,end) ~= classified_data(:,end));
+    error = p/size(real_data,1);
+    if error < min_error
+        best_structure = structure;
+        min_error = error;
+        depth = K(i);
+    end
+end
+structure = best_structure;
+[t_real_data, t_classified_data] = classify_test_data(test_data, structure);
+p = sum(t_real_data(:,end) ~= t_classified_data(:,end));
+test_error = p/size(t_real_data,1)
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PCA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ classes = cleaned_data(:,end);
+ cleaned_data = cleaned_data(:,1:end-1);
+wt = pca(cleaned_data);
+dim = [2, 3, 4, 5, 6, 7,8]
+for j = 1:length(dim)
+    %fprintf("--------------dim: %u------------------\n", dim(j)) 
+    X = cleaned_data * wt(:, 1:dim(j));
+    X = [X, classes];    
+    tvdata = X(1:600,:);% train and validate data
+    test_data = X(600:699,:);
+    min_error = 1;
+    for i = 1:length(K)
+        [result_tree, name, real_data, classified_data, structure] = classify_by_DT(tvdata, K(i));
+        disp(result_tree.tostring)
+        p = sum(real_data(:,end) ~= classified_data(:,end));
+        error = p / size(real_data,1);
+        fprintf("depth: %u, error: %.2f\n", K(i), error)
+        if error < min_error
+            best_structure = structure;
+            min_error = error;
+            depth = K(i);
+        end
+    end
+    fprintf("min error: %.2f, best depth: %u\n", min_error, depth);
+    structure = best_structure;
+    [t_real_data, t_classified_data] = classify_test_data(test_data, structure);
+    p = sum(t_real_data(:,end) ~= t_classified_data(:,end));
+    test_error = p/size(t_real_data,1)
+end
 
 
-% for i = 1:length(K)
-%     %K(i)
-%     [result_tree, name, real_data, classified_data, structure] = classify_by_DT(tvdata, K(i));
-%     %disp(result_tree.tostring);
-%     p = sum(real_data(:,10) ~= classified_data(:,10));
-%     error = p/size(real_data,1);
-%     [t_real_data, t_classified_data] = classify_test_data(test_data, structure);
-%     p = sum(t_real_data(:,10) ~= t_classified_data(:,10));
-%     test_error = p/size(t_real_data,1);
-% end
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function classes = k_nearest_neighbors(train_data, validate_data,k)
     classes = [];
     distances = [];
@@ -114,7 +159,8 @@ end
 function [temp , root, real_data, classified_data, structure] = classify_by_DT(data, max_depth, names, features, outs, classified_data, real_data ,depth, structure)
 
     if nargin < 3
-        names = ["ClumpThickness";"UniformityofCellSize";"UniformityofCellShape";"MarginalAdhesion";"SingleEpithelialCellSize";"BareNuclei";"BlandChromatin";"NormalNucleoli";"Mitoses"];
+        names = ["ClumpThickness";"UniformityofCellSize";"UniformityofCellShape";"MarginalAdhesion";"SingleEpithelialCellSize";"BareNuclei";"BlandChromatin";"NormalNucleoli";"Mitoses"];        
+        %names = ["PC1";"PC2";"PC3";"PC4";"PC5";"PC6";"PC7";"PC8"]; %When using pca
         features = data(:,1:size(data, 2)-1);
         outs = [];
         classified_data= [];
@@ -128,12 +174,14 @@ function [temp , root, real_data, classified_data, structure] = classify_by_DT(d
     
     impurity_measures = [];
     for i = 1: num_features
+        uniques = unique(ceil(data(:, i)));
+        num_uniques = length(uniques);
         if ~ismember(i,outs)
-            for j = 2:9
-                branch1 = data(data(:,i)<j,:);
-                branch2 = data(data(:,i)>=j,:);
+            for j = 1:num_uniques
+                branch1 = data(data(:,i)<uniques(j),:);
+                branch2 = data(data(:,i)>=uniques(j),:);
                 if size(branch1,1)~=0 & size(branch2,1)~=0 
-                    p = sum(branch1(:,10)==2);
+                    p = sum(branch1(:,end)==2);
                     p = p/size(branch1,1);
                     if p == 1 | p == 0
                         entropy = 0;
@@ -142,7 +190,7 @@ function [temp , root, real_data, classified_data, structure] = classify_by_DT(d
                     end
                     gini = 2*p*(1-p);
                     misclassification_error = 1- max(p,1-p);
-                    impurity_measures = [impurity_measures [i; j; entropy]];
+                    impurity_measures = [impurity_measures [i; uniques(j); entropy]];
                 end
             end
         end
@@ -150,13 +198,13 @@ function [temp , root, real_data, classified_data, structure] = classify_by_DT(d
     if size(impurity_measures,1)~=0
         [minimum col] = min(impurity_measures(3,:));
         impurity_measures(:,col);
-        outs = [outs impurity_measures(1,col)];
+        %outs = [outs impurity_measures(1,col)];
         branch1 = data(data(:,impurity_measures(1,col))< impurity_measures(2,col),:);
         branch2 = data(data(:,impurity_measures(1,col))>=impurity_measures(2,col),:);
     end
 
     % leaf
-    if size(data,1)<=10 ...
+    if size(data,1)<=100 ...
         | entropy < 0.01 ...
         |length(outs) == num_features ...
         | size(branch1,1) == size(data, 1) ...  
@@ -165,17 +213,19 @@ function [temp , root, real_data, classified_data, structure] = classify_by_DT(d
         
         [temp, root] = tree(data);
         real_data = [real_data ; data];
-        p1 = sum(data(:,10) == 2);
+        p1 = sum(data(:,end) == 2);
         p1 = p1/size(data,1);
-        p2 = sum(data(:,10) == 4);
+        p2 = sum(data(:,end) == 4);
         p2 = p2/size(data,1);
         class = ones(1,size(data,1));
         if p1 > p2
             class = 2*class; 
+            %fprintf('2');
         else
             class = 4*class; 
+            %fprintf('4');
         end
-        data(:,10) = class';
+        data(:,end) = class';
         classified_data = [classified_data ; data];
         
         structure = [structure, [0;0]];
@@ -195,6 +245,9 @@ function [temp , root, real_data, classified_data, structure] = classify_by_DT(d
     end
 end
  
+
+
+
 function [t_real_data, t_classified_data, col] = classify_test_data(data, structure, t_real_data, t_classified_data,col)
     if nargin < 3
         col = 1;
@@ -205,9 +258,9 @@ function [t_real_data, t_classified_data, col] = classify_test_data(data, struct
     end
     if structure(1,col) == structure(2,col) & structure(1,col)==0
         t_real_data = [t_real_data ; data];
-        p1 = sum(data(:,10) == 2);
+        p1 = sum(data(:,end) == 2);
         p1 = p1/size(data,1);
-        p2 = sum(data(:,10) == 4);
+        p2 = sum(data(:,end) == 4);
         p2 = p2/size(data,1);
         class = ones(1,size(data,1));
         if p1 > p2
@@ -215,14 +268,15 @@ function [t_real_data, t_classified_data, col] = classify_test_data(data, struct
         else
             class = 4*class; 
         end
-        data(:,10) = class';
+        data(:,end) = class';
         t_classified_data = [t_classified_data ; data];
         return
     end
     branch1 = data(data(:,structure(1,col))< structure(2,col),:);
     branch2 = data(data(:,structure(1,col))>=structure(2,col),:);
-    col;
     [t_real_data, t_classified_data, col] = classify_test_data(branch1, structure, t_real_data, t_classified_data,col);
     [t_real_data, t_classified_data, col] = classify_test_data(branch2, structure, t_real_data, t_classified_data,col);    
 
 end
+
+
